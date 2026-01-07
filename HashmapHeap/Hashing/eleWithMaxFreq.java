7f7745681ff9ecd0a79834018deed572259a8ce7
saivardhan1245 <mr.saivardhan@gmail.com>
2026-01-07 17:28:26 +0530

    added longest consecutive seq

---
+++ HashmapHeap/Hashing/eleWithMaxFreq.java
@@ -0,0 +1,27 @@
+package Arrays.Hashing;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class eleWithMaxFreq {
+    /*
+    Count Elements With Maximum Frequency
+    https://leetcode.com/problems/count-elements-with-maximum-frequency/description/
+     */
+    public int maxFrequencyElements(int[] nums) {
+        Map<Integer,Integer> map = new HashMap<>();int ans = 0 ;
+        for(int ele:nums){
+            map.put(ele,map.getOrDefault(ele,0)+1);
+        }
+        int maxfreq = 0 ;
+        for(int freq:map.values()){
+            maxfreq = Math.max(freq,maxfreq);
+        }
+        for(int freq:map.values()){
+            if(freq  == maxfreq){
+                ans += freq;
+            }
+        }
+        return ans;
+    }
+}
