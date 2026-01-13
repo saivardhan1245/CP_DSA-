e04f9dffd4767720e425c300ac44d92a0e1c0571
saivardhan1245 <mr.saivardhan@gmail.com>
2026-01-13 20:49:11 +0530

    subarray with o sum

---
+++ HashmapHeap/Hashing/HashmapHeap/longestsubarraywith0sum.java
@@ -0,0 +1,23 @@
+package HashmapHeap.Hashing.HashmapHeap;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class longestsubarraywith0sum {
+    //https://www.geeksforgeeks.org/problems/largest-subarray-with-0-sum/1
+    int maxLength(int arr[]) {
+        // code here
+        Map<Integer,Integer> map = new HashMap<>();
+        map.put(0,-1);
+        int maxLen = 0 , csum = 0 ;
+        for(int i = 0 ; i < arr.length ; i++){
+            csum+=arr[i];
+            if(!map.containsKey(csum)){
+                map.put(csum,i);
+            }else{
+                maxLen = Math.max(maxLen , i - map.get(csum));
+            }
+        }
+        return maxLen;
+    }
+}
