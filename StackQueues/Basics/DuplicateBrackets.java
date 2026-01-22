78a15e52ce34856997ab32967869a4c417e62914
saivardhan1245 <mr.saivardhan@gmail.com>
2026-01-23 00:38:39 +0530

    check redundancy

--- StackQueues/Basics/DuplicateBrackets.java
+++ StackQueues/Basics/DuplicateBrackets.java
@@ -5,7 +5,7 @@ import java.util.Stack;
 public class DuplicateBrackets{
      // https://www.geeksforgeeks.org/dsa/find-expression-duplicate-parenthesis-not/
     public static boolean checkDuplicate(String str){
-        Stack<Integer> st = new Stack<>();
+        Stack<Character> st = new Stack<>();
         for(char ch:str.toCharArray()){
             if(ch==')'){
                 if(st.peek()=='('){
@@ -16,6 +16,34 @@ public class DuplicateBrackets{
                 }
                 st.pop();
 
+            }else{
+                st.push(ch);
+            }
+        }
+        return false;
+    }
+    //https://www.geeksforgeeks.org/problems/expression-contains-redundant-bracket-or-not/1
+    public static boolean checkRedundancy(String s) {
+        // code here
+        Stack<Character> st = new Stack<>();
+        for(int i = 0 ; i < s.length() ; i++){
+            char ch = s.charAt(i);
+            if(ch==')'){
+                if(st.peek()=='('){
+                    return true;
+                }
+                boolean isOp = false;
+                while(st.peek()!='('){
+                    
+                    char chr = st.pop();
+                    if(chr=='+' || chr=='-' || chr=='*' || chr=='/'){
+                        isOp = true;
+                    }
+                }
+                st.pop();
+                if(!isOp){
+                    return true;
+                }
             }else{
                 st.push(ch);
             }
