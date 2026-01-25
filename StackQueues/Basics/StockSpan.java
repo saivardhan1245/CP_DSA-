be646576b60ca3aeb36d0398a2d5299607b3d43e
saivardhan1245 <mr.saivardhan@gmail.com>
2026-01-25 12:23:12 +0530

    stock spanner class problem

--- StackQueues/Basics/StockSpan.java
+++ StackQueues/Basics/StockSpan.java
@@ -22,6 +22,36 @@ public class StockSpan {
         }
         return ans;
     }
+
+    // stock span class
+   // https://leetcode.com/problems/online-stock-span/
+   class StockSpanner {
+    
+    Stack<int[]> st;
+    int idx;
+    public StockSpanner() {
+        st = new Stack<>();
+        idx = -1;
+    }
+    
+    public int next(int price) {
+        idx++;
+        int span = 0;
+        while(!st.isEmpty() && st.peek()[0]<=price){
+            st.pop();
+        }
+        
+         span  =  idx -  (st.isEmpty()? -1:st.peek()[1]);
+        st.push(new int[]{price,idx});
+        return span;
+    }
+}
+
+/**
+ * Your StockSpanner object will be instantiated and called as such:
+ * StockSpanner obj = new StockSpanner();
+ * int param_1 = obj.next(price);
+ */
     public static void main(String[] args) {
         
     }
