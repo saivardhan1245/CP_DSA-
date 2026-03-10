42b3ea356f6e3ee8d4d44dabf9a40ba828ef46f1
saivardhan1245 <mr.saivardhan@gmail.com>
2026-03-10 09:58:09 +0530

    minimum int to make valid ()

---
+++ StackQueues/parenthesis/miniAddValidString.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class  Minimum_Add_to_Make_Parentheses_Valid{
+    public int minAddToMakeValid(String s) {
+     
+        int open = 0 , openReq = 0 ;
+        for(char ch:s.toCharArray()){
+            if(ch=='('){
+                open++;
+            }else{
+                if(open>0){
+                    open--;
+                } else{
+                    openReq++;
+                }
+            }
+        }
+        return open + openReq;
+    }
+}
