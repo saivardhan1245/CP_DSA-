17f8e4cdf92a6231d23242a7cff220f5980f6f17
saivardhan1245 <mr.saivardhan@gmail.com>
2026-03-16 22:29:53 +0530

    remove k digits using greedy removal with stack ds

---
+++ StackQueues/Basics/removeKdigit.java
@@ -0,0 +1,40 @@
+//https://leetcode.com/problems/remove-k-digits/
+class removeKdigit {
+    public String removeKdigits(String num, int k) {
+          
+        int n = num.length();
+        Stack<Integer> st = new Stack<>();
+        for(int i = 0 ; i < n  ; i++){
+            int digit = num.charAt(i)-'0';
+            while(!st.isEmpty() && st.peek() > digit ){
+                
+                if(k>0){
+                    st.pop();
+                    k--;
+                }else{
+                    break;
+                }
+                   
+            }
+            st.push(digit);
+        }
+        while(!st.isEmpty() && k>0){
+            st.pop();
+            k--;
+        }
+        StringBuilder str = new StringBuilder();
+        while(!st.isEmpty()){
+            str.append(st.pop());
+        }
+        
+        int j = str.length()-1;
+        while(j>=0 && str.charAt(j) == '0' ){
+            str.deleteCharAt(j);
+            j--;
+        }
+        String res = str.reverse().toString();
+       
+        
+       return res.length()==0 ?"0":res;
+    }
+}
