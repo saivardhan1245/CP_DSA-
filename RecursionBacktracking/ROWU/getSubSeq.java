4c4b5d012db6b767464d36465a956e8ab9295e7a
saivardhan1245 <mr.saivardhan@gmail.com>
2026-06-11 14:26:18 +0530

    solved subseq of str using wayup

--- RecursionBacktracking/ROWU/getSubSeq.java
+++ RecursionBacktracking/ROWU/getSubSeq.java
@@ -1,9 +1,21 @@
-import java.util.*;
 
 public class getSubSeq {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String str = sc.next();
 
+    public static void printSubSeq(String str, String ssf) {
+        if (str.length() == 0) {
+            System.out.println(ssf);
+            return;
+        }
+        char ch = str.charAt(0);
+        String smallAns = str.substring(1);
+        // ch says no
+        printSubSeq(smallAns, ssf);
+        printSubSeq(smallAns, ssf + ch);
+    }
+
+    public static void main(String[] args) {
+
+        printSubSeq("abc", "");
+        // output [, c, b, bc, a, ac, ab, abc]
     }
 }
