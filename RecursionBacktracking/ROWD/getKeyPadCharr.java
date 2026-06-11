02721def2c996330744c9b5b3bd7c7e0b7a7be73
saivardhan1245 <mr.saivardhan@gmail.com>
2026-06-11 14:18:02 +0530

    solved problems related to ROWD

--- RecursionBacktracking/ROWD/getKeyPadCharr.java
+++ RecursionBacktracking/ROWD/getKeyPadCharr.java
@@ -1,33 +1,34 @@
-package RecursionBacktracking.ROWD;
 
 import java.util.ArrayList;
 import java.util.List;
 
 //https://leetcode.com/problems/letter-combinations-of-a-phone-number/
 public class getKeyPadCharr {
-    
-class Solution {
-    static String[] letters = {"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"}; 
-    public List<String> letterCombinations(String str) {
-    // base case;
-        if(str.length()==0){
-            List<String> bArr = new ArrayList<>();
-            bArr.add("");
-            return bArr;
-        }
-        String smallerstr = str.substring(1);
-        List<String> smallerAns = letterCombinations(smallerstr);
 
-        List<String> myAns = new ArrayList<>();
-        int fd = str.charAt(0)-'0';
-        String fl = letters[fd];
-        for(char ch:fl.toCharArray()){
-            for(String sa:smallerAns){
-                myAns.add(ch+sa);
+    class Solution {
+
+        static String[] letters = {"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+
+        public List<String> letterCombinations(String str) {
+            // base case;
+            if (str.length() == 0) {
+                List<String> bArr = new ArrayList<>();
+                bArr.add("");
+                return bArr;
             }
+            String smallerstr = str.substring(1);
+            List<String> smallerAns = letterCombinations(smallerstr);
+
+            List<String> myAns = new ArrayList<>();
+            int fd = str.charAt(0) - '0';
+            String fl = letters[fd];
+            for (char ch : fl.toCharArray()) {
+                for (String sa : smallerAns) {
+                    myAns.add(ch + sa);
+                }
+            }
+            return myAns;
         }
-        return myAns;
     }
-}
 
 }
--- RecursionBacktracking/ROWD/getMazePathswithJumps.java
+++ RecursionBacktracking/ROWD/getMazePathswithJumps.java
@@ -1,5 +1,39 @@
-package RecursionBacktracking.ROWD;
+
+import java.util.*;
 
 public class getMazePathswithJumps {
-    
+
+    public static List<String> getPathsJumps(int sr, int sc, int dr, int dc) {
+        if (sr > dr || sc > dc) {
+            List<String> barr = new ArrayList<>();
+            return barr;
+        }
+        if (sr == dr && sc == dc) {
+            List<String> barr = new ArrayList<>();
+            barr.add("");
+            return barr;
+        }
+        List<String> res = new ArrayList<>();
+        // horizontal jumps
+        for (int jumps = 1; jumps <= dc - sc; jumps++) {
+            List<String> afterHjumps = getPathsJumps(sr, sc + jumps, dr, dc);
+            for (String c : afterHjumps) {
+                res.add("h" + jumps + c);
+            }
+        }
+        // vertical jumps
+        for (int jumps = 1; jumps <= dr - sr; jumps += 1) {
+            List<String> afterVjumps = getPathsJumps(sr + jumps, sc, dr, dc);
+            for (String c : afterVjumps) {
+                res.add("v" + jumps + c);
+            }
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(getPathsJumps(0, 0, 2, 2));
+        //[h1h1v1v1, h1h1v2, h1v1h1v1, h1v1v1h1, h1v2h1, h2v1v1, h2v2, 
+        //v1h1h1v1, v1h1v1h1, v1h2v1, v1v1h1h1, v1v1h2, v2h1h1, v2h2]
+    }
 }
