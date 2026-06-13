031580bb624fd26bb8df9ed4929292c1c68e45e7
saivardhan1245 <mr.saivardhan@gmail.com>
2026-06-13 11:17:26 +0530

    solved print permutations

--- RecursionBacktracking/RecursionProblems/printPermutations.java
+++ RecursionBacktracking/RecursionProblems/printPermutations.java
@@ -1,5 +1,21 @@
-package RecursionBacktracking.RecursionProblems;
+// package RecursionBacktracking.RecursionProblems;
 
 public class printPermutations {
-    
+
+    public static void printPerm(String str, String asf) {
+        if (str.length() == 0) {
+            System.out.print(asf + " ");
+            return;
+        }
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+            String rem = str.substring(0, i) + str.substring(i + 1);
+            printPerm(rem, asf + ch);
+        }
+    }
+
+    public static void main(String[] args) {
+        String str = "abc";
+        printPerm(str, "");
+    }
 }
