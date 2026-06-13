8cfb1a9ee9edb566be72eedc5aa4597459a780ce
saivardhan1245 <mr.saivardhan@gmail.com>
2026-06-13 10:48:09 +0530

    solved encodings of a string

--- RecursionBacktracking/RecursionProblems/printEncodings.java
+++ RecursionBacktracking/RecursionProblems/printEncodings.java
@@ -1,5 +1,40 @@
-package RecursionBacktracking.RecursionProblems;
 
 public class printEncodings {
-    
+
+    public static char digitToString(int digit) {
+        return (char) ((digit - 1) + 'a');
+    }
+
+    public static void printEncoding(String str, String asf) {
+        if (str.length() == 0) {
+            System.out.println(asf);
+            return;
+        }
+        int ch = str.charAt(0) - '0';
+        if (ch == 0) {
+            return;
+        }
+        char digit = digitToString(ch);
+        String nextStr = str.substring(1);
+        printEncoding(nextStr, asf + digit);
+
+        if (str.length() >= 2) {
+            String ss = str.substring(0, 2);
+            int d = Integer.parseInt(ss);
+            if (d <= 26) {
+                char c = digitToString(d);
+                String t = str.substring(2);
+                printEncoding(t, asf + c);
+            }
+        }
+
+    }
+
+    public static void main(String[] args) {
+        String curr = "123";
+        printEncoding(curr, "");
+        /**
+         * abc aw lc
+         */
+    }
 }
