e214a065ff4af6fe8a70444ea245fb4f52363918
saivardhan1245 <mr.saivardhan@gmail.com>
2026-07-04 12:14:28 +0530

    solved coin change permuations

--- RecursionBacktracking/RecursionProblems/CoinChangePermuations.java
+++ RecursionBacktracking/RecursionProblems/CoinChangePermuations.java
@@ -1,5 +1,29 @@
 package RecursionBacktracking.RecursionProblems;
 
 public class CoinChangePermuations {
-    
+
+    public static void ccpOneCoin(int[] arr, int tar, boolean[] vis, String asf) {
+        if (tar == 0) {
+            System.out.println(asf);
+            return;
+        }
+        if (tar < 0) {
+            return;
+        }
+        for (int i = 0; i < arr.length; i++) {
+            if (!vis[i]) {
+                vis[i] = true;
+                ccpOneCoin(arr, tar - arr[i], vis, asf + arr[i] + " ");
+                vis[i] = false;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {2, 3, 4, 6, 5};
+        int tar = 10;
+        boolean[] vis = new boolean[5];
+        ccpOneCoin(arr, tar, vis, "");
+
+    }
 }
--- RecursionBacktracking/RecursionProblems/FloodFIll.java
+++ RecursionBacktracking/RecursionProblems/FloodFIll.java
@@ -1,5 +1,26 @@
 package RecursionBacktracking.RecursionProblems;
 
 public class FloodFIll {
+    public static void printFloodFill(int r,int c ,int[][] mat,boolean[][] vis,String asf){
+        if(r==mat.length && c == mat[0].length){
+            System.out.println(asf);
+            return;
+        }
+        if(r<0 || c<0 ||vis[r][c] || mat[r][c]==1 || r>=mat.length || c>=mat[0].length){
+            return;
+        }
+        vis[r][c] = true;
+        //tldr
+        printFloodFill(r-1,c,mat,vis,asf+"t");
+        printFloodFill(r,c-1,mat,vis,asf+"l");
+        printFloodFill(r+1,c,mat,vis,asf+"d");
+        printFloodFill(r,c+1,mat,vis,asf+"r");
+        vis[r][c] = false; //backtracking
     
+    }
+    public static void main(String[] args){
+        int[][] matrix = {{},{},{}};
+        boolean[][] vis = new boolean[matrix.length][matrix[0].length];
+        printFloodFill(0,0,matrix,vis,"");
+    }
 }
